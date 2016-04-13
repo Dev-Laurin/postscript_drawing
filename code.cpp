61702d35e0b0a67a07fbdcaeaaf2bf32ceb0f3d7
Parker <fishing.setnet@gmail.com>
2016-04-13 15:56:36 -0800

    added polygon

--- code.cpp
+++ code.cpp
@@ -90,7 +90,7 @@ public:
         _hitY=_hight/2;
     }
     void print(ostream& out) override{
-        out << ".01 setlinewidth\nnewpath\n"
+        out << "newpath\n"
             << -.5*_width << " " << -.5*_hight << " moveto\n"
             << -.5*_width << " " << .5*_hight << " lineto\n"
             << .5*_width << " " << .5*_hight << " lineto\n"
@@ -133,19 +133,35 @@ private:
 };
 
 class polygon: public shape{
+public:
     polygon(int sides,double length){
         _sides=sides;
-        double angle=2*3.1415/_sides;
-        
+        _angle=2*3.1415/_sides;
+        _radious=sin(_angle/2)*2*length;
+        _hitX=_radious;
+        _hitY=_radious;
+    }
+    void print(ostream& out) override{
+        out << "newpath\n"
+            << "0 " << _radious << " moveto\n";
+            for(int i=1;i<_sides;i++){
+                printPoint(out,i);
+            }
+        out << "closepath\nstroke\n";
     }
 private:
+    void printPoint(ostream& out,int N){
+        double pointAngle=3.1415/2+N*_angle;
+        out << _radious*cos(pointAngle) << " " << _radious*sin(pointAngle) << " lineto\n";
+    }
     double _radious;
+    double _angle;
     int _sides;
-}
+};
 
 int main(){
     ofstream out("output.ps");
-    circle cir(40);
+    polygon cir(5,40);
     cir.print(out);
 /*
     rectangle rec(1,1);
